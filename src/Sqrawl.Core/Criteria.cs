c968e3f4541c1592c2320835469d56521bd8396b
Aaron Lord <aaron@lorddesign.net>
2017-04-03 17:20:51 -0700

    Stabilize the build

--- src/Sqrawl.Core/Criteria.cs
+++ src/Sqrawl.Core/Criteria.cs
@@ -1,34 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Sqrawl.Core
 {
     public class Criteria<T>
     {
-        private Column<T> _target;
         private T _matchValue;
-        private SelectQuery _query;
+        private readonly SelectQuery _query;
+        private readonly Column _target;
 
-        internal Criteria(SelectQuery query, Column<T> target)
+        internal Criteria(SelectQuery query, Column target)
         {
             _query = query;
             _target = target;
         }
 
+        // TODO: Need to rename this so it doesn't get confused with Object.Equals()
         public SelectQuery Equals(T value)
         {
             _matchValue = value;
-            _query.Criteria.Add($"@{_target.Name.ToLower()}_{Guid.NewGuid().ToString("N").Substring(0, 8)}", value);
+            _query.Criteria.Add($"@{_target.Name.ToLowerInvariant()}_{Guid.NewGuid().ToString("N").Substring(0, 8)}", value);
             return _query;
         }
     }
 
-     public class Column<T> { }
-
     public static class CriteriaExtensions
     {
-        public static Criteria<T> Where<T>(this Table table, Column<T> column)
+        public static Criteria<T> Where<T>(this Table table, Column column)
         {
             return new Criteria<T>(table.Query, column);
         }
